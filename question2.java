fb1301438e732ab56ecaf50b57ee6ca628759194
Vishnu Dattu Lakkaraju <lvdattu2003@gmail.com>
2025-11-12 11:41:03 +0530

    Create question2.java
    Sum of All Numbers

---
+++ question2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    
+    public static int sumOfDigits(int n) {
+        if (n == 0)
+            return 0;
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = sumOfDigits(num);
+        System.out.println(sum);
+        sc.close();
+    }
+}
